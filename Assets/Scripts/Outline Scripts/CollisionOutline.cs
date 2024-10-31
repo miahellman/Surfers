a8f7c6916bee65360fd8f22f0eeb488e2e0172aa
miahellman <hellman.mia@gmail.com>
2024-10-31 00:28:59 -0400

    uploaded outline shader, made terrain blockout much smaller

---
+++ Assets/Scripts/Outline Scripts/CollisionOutline.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CollisionOutline : MonoBehaviour
+{
+    // Outline object
+    private Renderer outlinedObject;
+
+    // Materials for outline vs no outline
+    public Material WriteObject;
+    public Material ApplyOutline;
+
+    // Triggered when the player collides with a selectable object
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the collided object is tagged "Selectable"
+        if (other.CompareTag("Selectable"))
+        {
+            outlinedObject = other.GetComponent<Renderer>();
+            Debug.Log("Collided with selectable object");
+        }
+    }
+
+    // Triggered when the player exits collision with a selectable object
+    private void OnTriggerExit(Collider other)
+    {
+        // If exiting a selectable object, remove the outline
+        if (other.CompareTag("Selectable"))
+        {
+            outlinedObject = null;
+            Debug.Log("Exited selectable object");
+        }
+    }
+
+    // Called after all rendering is complete, draws the outline
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        // Setup CommandBuffer for rendering
+        var commands = new CommandBuffer();
+        int selectionBuffer = Shader.PropertyToID("_SelectionBuffer");
+        commands.GetTemporaryRT(selectionBuffer, source.descriptor);
+
+        // Render selection buffer
+        commands.SetRenderTarget(selectionBuffer);
+        commands.ClearRenderTarget(true, true, Color.clear);
+
+        // Draw outline if an object is selected
+        if (outlinedObject != null)
+        {
+            commands.DrawRenderer(outlinedObject, WriteObject);
+        }
+
+        // Apply everything and clean up in CommandBuffer
+        commands.Blit(source, destination, ApplyOutline);
+        commands.ReleaseTemporaryRT(selectionBuffer);
+
+        // Execute and clean up CommandBuffer itself
+        Graphics.ExecuteCommandBuffer(commands);
+        commands.Dispose();
+    }
+}
---
+++ Assets/Scripts/Outline Scripts/ObjectOutline.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class ObjectOutline : MonoBehaviour
+{
+	//outline object
+	public Renderer OutlinedObject;
+
+	//materials for outline vs no outline
+	public Material WriteObject;
+	public Material ApplyOutline;
+
+	void Update()
+	{
+		//check if we clicked on a selectable object
+		if (Input.GetMouseButtonDown(0))
+		{
+			var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			bool hitSelectable = Physics.Raycast(ray, out var hit) && hit.transform.CompareTag("Selectable");
+			//if hit, get the renderer of hit object, else no outline at all
+			if (hitSelectable) {
+				OutlinedObject = hit.transform.GetComponent<Renderer>();
+			} else {
+				OutlinedObject = null;
+			}
+		}
+	}
+
+	void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+		//setup stuff
+		var commands = new CommandBuffer();
+		int selectionBuffer = Shader.PropertyToID("_SelectionBuffer");
+		commands.GetTemporaryRT(selectionBuffer, source.descriptor);
+		//render selection buffer
+		commands.SetRenderTarget(selectionBuffer);
+		commands.ClearRenderTarget(true, true, Color.clear);
+		//draws outline if object is selected
+		if (OutlinedObject != null)
+		{
+			commands.DrawRenderer(OutlinedObject, WriteObject);
+		}
+		//apply everything and clean up in commandbuffer
+		commands.Blit(source, destination, ApplyOutline);
+		commands.ReleaseTemporaryRT(selectionBuffer);
+		
+		//execute and clean up commandbuffer itself
+		Graphics.ExecuteCommandBuffer(commands);
+		commands.Dispose();
+	}
+	
+}
